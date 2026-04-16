7f861a8af162e17758bbe2b20f00a3f3d89dbee1
Mads Brouer Lundholm <mads@madslundholm.dk>
2026-04-16 19:55:28 +0000

    fix: jsonencode the asume role statement

--- bootstrap/aws.tf
+++ bootstrap/aws.tf
@@ -9,7 +9,7 @@ resource "aws_iam_role" "spacelift_integration" {
   assume_role_policy = core::jsonencode({
     Version = "2012-10-17",
     Statement = [
-      data.spacelift_aws_integration_attachment_external_id.control_plane.assume_role_policy_statement
+      core::jsonencode(data.spacelift_aws_integration_attachment_external_id.control_plane.assume_role_policy_statement)
     ]
   })
 }
