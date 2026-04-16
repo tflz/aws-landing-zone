dd4bb0df7d3f3a01f14708c18e414e2dbbeae12f
Mads Brouer Lundholm <mads@madslundholm.dk>
2026-04-16 19:41:02 +0000

    feature: create iam role in aws

--- bootstrap/aws.tf
+++ bootstrap/aws.tf
@@ -1,3 +1,20 @@
 data "aws_partition" "current" {}
 
 data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "spacelift_integration" {
+  name = "control_plane"
+  path = "${var.landing_zone_name}/spacelift"
+
+  assume_role_policy = core::jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      data.spacelift_aws_integration_attachment_external_id.control_plane.assume_role_policy_statement
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "control_plane" {
+  role = aws_iam_role.spacelift_integration.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
--- bootstrap/spacelift.tf
+++ bootstrap/spacelift.tf
@@ -3,7 +3,7 @@ resource "spacelift_space" "landing_zone" {
   parent_space_id = var.spacelift_parent_space_id
 }
 
-resource "spacelift_stack" "control_plan" {
+resource "spacelift_stack" "control_plane" {
   name       = "${var.landing_zone_name}-control-plan"
   autodeploy = true
 
@@ -15,4 +15,11 @@ resource "spacelift_stack" "control_plan" {
 resource "spacelift_aws_integration" "control_plane" {
   name     = "${var.landing_zone_name}-aws-integration"
   role_arn = "arn:${data.aws_partition.current.id}:iam::${data.aws_caller_identity.current.account_id}:role/${var.landing_zone_name}-control-plan"
+}
+
+data "spacelift_aws_integration_attachment_external_id" "control_plane" {
+  integration_id = spacelift_aws_integration.control_plane.id
+  stack_id       = spacelift_stack.control_plan.id 
+  read           = true
+  write          = true
 }
