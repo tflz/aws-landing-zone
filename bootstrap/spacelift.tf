795d147a16da390a82f9d537aff4232f64a0a88c
Mads Brouer Lundholm <mads@madslundholm.dk>
2026-04-16 19:19:47 +0000

    feature: create the control plane stack

--- bootstrap/spacelift.tf
+++ bootstrap/spacelift.tf
@@ -1,4 +1,18 @@
-resource "spacelift_aws_integration" "control_plane_integration" {
+resource "spacelift_space" "landing_zone" {
+  name            = var.landing_zone_name
+  parent_space_id = var.spacelift_parent_space_id
+}
+
+resource "spacelift_stack" "control_plan" {
+  name       = "${var.landing_zone_name}-control-plan"
+  autodeploy = true
+
+  repository = var.github_repository_name
+  branch     = var.github_repository_branch
+  project_root = "stacks/control-plane"
+}
+
+resource "spacelift_aws_integration" "control_plane" {
   name     = "${var.landing_zone_name}-aws-integration"
   role_arn = "arn:${data.aws_partition.current.id}:iam::${data.aws_caller_identity.current.account_id}:role/${var.landing_zone_name}-control-plan"
 }
--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -2,3 +2,20 @@ variable "landing_zone_name" {
   type = string
   description = "The display name of the landing zone."
 }
+
+variable "spacelift_parent_space_id" {
+  type = string
+  description = "ID of the parent space where the landing zone will be deployed. Defaults to root"
+  default = "root"
+}
+
+variable "github_repository_name" {
+  type = string
+  description = "The name of the repository without owner part."
+}
+
+variable "github_repository_branch" {
+  type = string
+  description = "Git branch to apply changes from. Defaults to main."
+  default = "main"
+}
